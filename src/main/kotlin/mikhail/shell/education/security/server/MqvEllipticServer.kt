a44c99f6a9b53980eee6a7383133be3f0fcbf632
Mikhail_Shell <theoutstandingme@gmail.com>
2025-04-08 19:43:14 +0700

    now key is generated and I can immediately send data

--- src/main/kotlin/mikhail/shell/education/security/server/MqvEllipticServer.kt
+++ src/main/kotlin/mikhail/shell/education/security/server/MqvEllipticServer.kt
@@ -7,10 +7,11 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.delay
 import java.math.BigInteger
 import java.time.Duration
 
-class MqvEllipticServer: BaseEllipticServer() {
+open class MqvEllipticServer: BaseEllipticServer() {
     var xU: BigInteger? = null
     var yU: BigInteger? = null
     var xV: BigInteger? = null
@@ -19,57 +20,70 @@ class MqvEllipticServer: BaseEllipticServer() {
     val yUdef = CompletableDeferred<BigInteger>()
     val xVdef = CompletableDeferred<BigInteger>()
     val yVdef = CompletableDeferred<BigInteger>()
+    protected val userIDset = mutableSetOf<String>()
     override fun start() {
         embeddedServer(Netty, port = 9876){
             install(WebSockets.Plugin) {
                 pingPeriod = Duration.ofSeconds(10)
             }
             routing {
-                webSocket("/handshake") {
-                    val clientName = (incoming.receive() as Frame.Text).readText()
-                    sendNumber(p)
-                    sendNumber(q)
-                    sendNumber(a)
-                    sendNumber(b)
-                    sendNumber(xG)
-                    sendNumber(yG)
-                    if (clientName == "CATHY") {
-                        xC = receivePublicKey()
-                        yC = receivePublicKey()
-                        xCdef.complete(xC!!)
-                        yCdef.complete(yC!!)
-                        xF = xFdef.await()
-                        yF = yFdef.await()
-                        sendNumber(xF!!)
-                        sendNumber(yF!!)
-                        xU = receivePublicKey()
-                        yU = receivePublicKey()
-                        xUdef.complete(xU!!)
-                        yUdef.complete(yU!!)
-                        xV = xVdef.await()
-                        yV = yVdef.await()
-                        sendNumber(xV!!)
-                        sendNumber(yV!!)
-                    } else if (clientName == "FRED") {
-                        xF = receivePublicKey()
-                        yF = receivePublicKey()
-                        xFdef.complete(xF!!)
-                        yFdef.complete(yF!!)
-                        xC = xCdef.await()
-                        yC = yCdef.await()
-                        sendNumber(xC!!)
-                        sendNumber(yC!!)
-                        xV = receivePublicKey()
-                        yV = receivePublicKey()
-                        xVdef.complete(xV!!)
-                        yVdef.complete(yV!!)
-                        xU = xUdef.await()
-                        yU = yUdef.await()
-                        sendNumber(xU!!)
-                        sendNumber(yU!!)
-                    }
-                }
+                setupEndpoints(this)
             }
         }.start(wait = true)
+    }
+
+    protected open fun setupEndpoints(route: Route) {
+        route.webSocket("/handshake") {
+            if (userIDset.size == 2) {
+                close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Room is full."))
+                return@webSocket
+            }
+            val clientID = (incoming.receive() as Frame.Text).readText()
+            userIDset.add(clientID)
+            while(userIDset.size < 2) {
+                delay(1000)
+            }
+            sendNumber(p)
+            sendNumber(q)
+            sendNumber(a)
+            sendNumber(b)
+            sendNumber(xG)
+            sendNumber(yG)
+            if (clientID == userIDset.first()) {
+                xC = receivePublicKey()
+                yC = receivePublicKey()
+                xCdef.complete(xC!!)
+                yCdef.complete(yC!!)
+                xF = xFdef.await()
+                yF = yFdef.await()
+                sendNumber(xF!!)
+                sendNumber(yF!!)
+                xU = receivePublicKey()
+                yU = receivePublicKey()
+                xUdef.complete(xU!!)
+                yUdef.complete(yU!!)
+                xV = xVdef.await()
+                yV = yVdef.await()
+                sendNumber(xV!!)
+                sendNumber(yV!!)
+            } else if (clientID == userIDset.last()) {
+                xF = receivePublicKey()
+                yF = receivePublicKey()
+                xFdef.complete(xF!!)
+                yFdef.complete(yF!!)
+                xC = xCdef.await()
+                yC = yCdef.await()
+                sendNumber(xC!!)
+                sendNumber(yC!!)
+                xV = receivePublicKey()
+                yV = receivePublicKey()
+                xVdef.complete(xV!!)
+                yVdef.complete(yV!!)
+                xU = xUdef.await()
+                yU = yUdef.await()
+                sendNumber(xU!!)
+                sendNumber(yU!!)
+            }
+        }
     }
 }
--- src/main/kotlin/mikhail/shell/education/security/server/estream/EStreamServer.kt
+++ src/main/kotlin/mikhail/shell/education/security/server/estream/EStreamServer.kt
@@ -8,55 +8,46 @@ import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
-import mikhail.shell.education.security.server.Server
+import mikhail.shell.education.security.server.MqvEllipticServer
 
-class EStreamServer: Server {
+class EStreamServer: MqvEllipticServer() {
     private companion object {
         const val BUFFER_SIZE = 1024
     }
     private val sessions = mutableSetOf<DefaultWebSocketSession>()
-    override fun start() {
-        embeddedServer(
-            factory = Netty,
-            port = 9999
-        ) {
-            install(WebSockets) {
-                masking = false
+    override fun setupEndpoints(route: Route) {
+        super.setupEndpoints(route)
+        route.webSocket("/transfer") {
+            if (!sessions.contains(this) && sessions.size < 2) {
+                sessions.add(this)
+            } else {
+                close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Room is full."))
+                return@webSocket
             }
-            routing {
-                webSocket("/transfer") {
-                    if (!sessions.contains(this) && sessions.size < 2) {
-                        sessions.add(this)
-                    } else {
-                        close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT, "Room is full."))
-                        return@webSocket
-                    }
-                    while (sessions.size < 2) {
-                        delay(1000)
-                    }
-                    while (sessions.size == 2) {
-                        val anotherSession = sessions.first { it != this }
-                        val fileSizeFrame = incoming.receive()
-                        anotherSession.outgoing.send(fileSizeFrame)
-                        val fileSize = (fileSizeFrame as Frame.Text).readText().toInt()
-                        val fileNameFrame = incoming.receive()
-                        anotherSession.send(fileNameFrame)
-                        var bytesLeft = fileSize
-                        while (bytesLeft > 0) {
-                            val ivFrame = incoming.receive()
-                            val byteFrame = incoming.receive()
-                            anotherSession.outgoing.send(ivFrame)
-                            anotherSession.outgoing.send(byteFrame)
-                            bytesLeft -= BUFFER_SIZE
-                        }
-                        sessions.forEach {
-                            if (!it.isActive) {
-                                sessions.remove(it)
-                            }
-                        }
+            while (sessions.size < 2) {
+                delay(1000)
+            }
+            while (sessions.size == 2) {
+                val anotherSession = sessions.first { it != this }
+                val fileSizeFrame = incoming.receive()
+                anotherSession.outgoing.send(fileSizeFrame)
+                val fileSize = (fileSizeFrame as Frame.Text).readText().toInt()
+                val fileNameFrame = incoming.receive()
+                anotherSession.send(fileNameFrame)
+                var bytesLeft = fileSize
+                while (bytesLeft > 0) {
+                    val ivFrame = incoming.receive()
+                    val byteFrame = incoming.receive()
+                    anotherSession.outgoing.send(ivFrame)
+                    anotherSession.outgoing.send(byteFrame)
+                    bytesLeft -= BUFFER_SIZE
+                }
+                sessions.forEach {
+                    if (!it.isActive) {
+                        sessions.remove(it)
                     }
                 }
             }
-        }.start(wait = true)
+        }
     }
 }
